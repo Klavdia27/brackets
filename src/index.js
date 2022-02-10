e7c3520aa3df9212b8c9309922958549a339f703
Klavdia Petukhova <kklavdia27@gmail.com>
2022-02-10 13:37:28 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -5,8 +5,25 @@ module.exports = function check(str, bracketsConfig) {
   console.log(arrOpen[0] === str[0]);
   
   if (str.length % 2 === 1) { return false}
+ 
+  let arr = [];
+  for ( elem of str) {
+    //console.log(elem)
+    if (elem === "(" && elem === "{" && elem === "[") { arr.push(elem)}
+    if (elem === ")") {
+      if (elem-1 === "(") { arr.pop()}
+    }
+    if (elem === "}") {
+      if (elem-1 === "{") { arr.pop()}
+    }
+    if (elem === "]") {
+      if (elem-1 === "[") { arr.pop()}
+    }
+  
 
 
+  }
+ if (arr.length === 0) {return true} 
 
 
 
