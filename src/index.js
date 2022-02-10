787d0d31fd5acb4fc88f93a7a3a0fdf189ac63ce
Klavdia Petukhova <kklavdia27@gmail.com>
2022-02-10 13:03:54 +0300

    feat: add task braskets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  console.log('1=',str);
+  const arrOpen = ["(","{","[","|"];
+  const arrClose = [")","}","]","|"];
+  console.log(arrOpen[0] === str[0]);
+  
+  if (str.length % 2 === 1) { return false}
+
+
+
+
+
 }
